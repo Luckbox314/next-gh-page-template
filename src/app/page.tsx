4d961f843ecb7a724820d549ea16c2ff9e9095ff
Lucas Muñoz <50024324+Luckbox314@users.noreply.github.com>
2024-12-18 20:37:34 -0300

    test: actions test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>Template</h1>
+      <h1>Template test</h1>
     </div>
   );
 }
